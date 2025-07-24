aa5f17db87ebdce25ae3f55cd49e954c2e3c698c
Bart Massey <bart.massey@gmail.com>
2025-07-24 11:31:19 -0700

    factored out trie module

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use std::path::PathBuf;
 
 use anyhow::Error;
 
+mod trie;
+use trie::{Node, letters_in};
+
 fn main() -> Result<(), Error> {
     let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     path.push("usa_5.txt");
@@ -79,72 +82,3 @@ impl<'d> Cell<'d> {
     }
 }
 
-#[derive(Default)]
-struct Node {
-    members: u32,
-    leaves: u32,
-    children: Vec<Node>,
-}
-
-impl Node {
-    fn insert(&mut self, s: &str) {
-        let mut node = self;
-        for ch in s.chars() {
-            node.leaves += 1;
-            let (bit, index) = node.bit_and_index(ch);
-            if node.members & bit == 0 {
-                node.children.insert(index, Self::default());
-                node.members |= bit;
-            }
-            node = &mut node.children[index];
-        }
-    }
-
-    fn follow(&self, ch: char) -> Option<&Node> {
-        let (bit, index) = self.bit_and_index(ch);
-        if self.members & bit == 0 {
-            return None;
-        }
-        Some(&self.children[index])
-    }
-
-    fn bit_and_index(&self, ch: char) -> (u32, usize) {
-        let bit = 1 << letter_index(ch);
-        let index = (self.members & (bit - 1)).count_ones() as usize;
-        (bit, index)
-    }
-}
-
-fn letter_index(ch: char) -> u32 {
-    assert!(ch.is_ascii_alphabetic());
-    ch.to_ascii_uppercase() as u32 - b'A' as u32
-}
-
-fn letters_in(mut mask: u32) -> impl Iterator<Item = char> {
-    std::iter::from_fn(move || {
-        if mask == 0 {
-            return None;
-        }
-        let index = (mask & !(mask - 1)).trailing_zeros();
-        mask = mask & (mask - 1);
-        Some(char::from_u32(b'A' as u32 + index).unwrap())
-    })
-}
-
-#[test]
-fn insertions() {
-    let mut root = Node::default();
-    root.insert("foo");
-    root.insert("bar");
-    root.insert("baz");
-
-    assert_eq!(
-        root.members,
-        (1 << letter_index('f')) | (1 << letter_index('b'))
-    );
-    assert_eq!(root.leaves, 3);
-    assert_eq!(root.children[0].members, (1 << letter_index('a')));
-    assert_eq!(root.children[0].leaves, 2);
-    assert_eq!(root.children[1].members, (1 << letter_index('o')));
-    assert_eq!(root.children[1].leaves, 1);
-}
